80807365154da597299727b71bb430196cec2c7e
Anmol <anmolshetty256@gmail.com>
2026-02-09 10:54:57 +0530

    changes to cors access on backend

--- server/index.js
+++ server/index.js
@@ -85,7 +85,9 @@ const allowedOrigins = [
     'https://vexstorm26.datavex.ai',
     'http://localhost:5173',
     'http://localhost:5500',
-    'http://127.0.0.1:5500'
+    'http://127.0.0.1:5500',
+    'https://datavex.ai',
+    'https://www.datavex.ai'
 ].filter(Boolean);
 
 app.use(cors({
